49ef1d43042d80171e1f123b217d1c9e124c2885
Bollash <scheierbalu@gmail.com>
2021-03-20 00:46:29 +0100

    space comments

--- src/Space.java
+++ src/Space.java
@@ -3,18 +3,45 @@ import java.util.List;
 import java.util.Random;
 
 public class Space {
+
+    /**
+     * Aktív telepesek száma
+     */
     private int aliveSettlerCnt;
 
+    /**
+     * Napszél visszaszámláló
+     */
     private int turnsTillSunStorm;
 
+    /**
+     * Játék állapotát jelző flag.
+     * Ha True és több mint aktív telepes van akkor a játékosok nyertek
+     * Ellenkező esetben vesztettek.
+     */
     private boolean gameOver;
 
+    /**
+     * Hány körönként jön a napszél
+     */
     private int sunStormFreq;
 
+    /**
+     * Az aszteroidák gyűjteménye
+     */
     private List<Asteroid> asteroids;
 
+    /**
+     * Aktív karakterek gyűjteménye
+     */
     private List<Character> characters;
 
+    /**
+     * kapott paraméterek alapján legenerál egy játékteret
+     * @param settlerCnt Játékosok száma
+     * @param sameCnt Egy bizonyos nyersanyag magú aszteroidák száma. Összesen 4x ennyi aszteroida lesz a játékban.
+     * @param freq Napvihar frekvencia.
+     */
     public Space(int settlerCnt, int sameCnt, int freq){
         gameOver = false;
         this.aliveSettlerCnt = settlerCnt;
@@ -46,10 +73,17 @@ public class Space {
         }
     }
 
+    /**
+     * Karakter kivevése az aktív karakterek közül
+     * @param character Ez a karakter halt meg
+     */
     public void removeCharacter(Character character){
         characters.remove(character);
     }
 
+    /**
+     * Csükkenti a napviharszámlálót. Ha az elérte a nullát, akkor napvihar söpör végig a pályán, és a számláló a sunStormFreq értékre áll vissza.
+     */
     public void handleCountDown(){
         turnsTillSunStorm--;
 
@@ -61,24 +95,44 @@ public class Space {
         }
     }
 
+    /**
+     * Kapott karaktert hozzá adja az aktív játékosok közé
+     * @param character Ez a karakter kerül a characters-be
+     */
     public void addCharacter(Character character){
         if(!characters.contains(character)){
             characters.add(character);
         }
     }
 
+    /**
+     * gameOver flag getter-e
+     * @return _
+     */
     public boolean isGameOver() {
         return gameOver;
     }
 
+    /**
+     * gameOver flag setter-e
+     * @param gameOver _
+     */
     public void setGameOver(boolean gameOver) {
         this.gameOver = gameOver;
     }
 
+    /**
+     * aliveSettlerCnt getter-e
+     * @return .
+     */
     public int getAliveSettlerCnt() {
         return aliveSettlerCnt;
     }
 
+    /**
+     * aliveSettlerCnt setter-e
+     * @param aliveSettlerCnt _
+     */
     public void setAliveSettlerCnt(int aliveSettlerCnt) {
         this.aliveSettlerCnt = aliveSettlerCnt;
     }
