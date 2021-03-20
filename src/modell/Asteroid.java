9ecbca1c5bd97ccf26e77e81d9877d5ee3b740c9
koflanovicsk <kristof.koflanovics@gmail.com>
2021-03-20 14:49:52 +0100

    Asteroidbol még a countMaterialsOnSurface es a getDrillableNeighbour hianyzik.

--- src/modell/Asteroid.java
+++ src/modell/Asteroid.java
@@ -30,22 +30,102 @@ public class Asteroid implements IAsteroid {
         coreMaterial = core;
     }
 
-    public void getDrilled(){}
+    /**
+     * A kéreg mérete 1-gyel csökken, ha pedig már 0 a méret, akkor nem történik semmi.
+     */
+    public void getDrilled(){
+        if(layer == 0){
+            return;
+        }
+        else {
+            layer = layer-1;
+        }
 
-    public Material getMined()throws CantBeMinedException {}
+    }
+    /**
+    * Ha már át lett fúrva a kéreg, akkor kiveszi a materialt és a meghívó megkapja azt.
+     */
+    public Material getMined()throws CantBeMinedException {
+            if (layer == 0) {
+                return coreMaterial;
+            }
+            else{
+                throw new CantBeMinedException("Layer is not 0");
+            }
 
-    public void addNeighbour(IAsteroid neighbour){}
+    }
 
-    public MaterialArray countMaterialOnSurface(){}
+    /**
+     * Ha a  paraméterként kapott objektum még nincs a szomszédok között, akkor bekerül oda.
+     */
+    public void addNeighbour(IAsteroid neighbour){
+        if (!neighbours.contains(neighbour)){
+            neighbours.add(neighbour);
+        }
+    }
 
-    public void addCore(Material material)throws CoreFullException {}
+    /**
+     * Az aszteroidán lévő karakterek nyersanyagait összegyűjti és visszatér egy MaterialArray-el
+     * amiben ezek vannak.
+     */
+    //TODO
+    public MaterialArray countMaterialOnSurface(){
+        MaterialArray materialArray = new MaterialArray();
+        for (Character character : charactersOnSurface) {
 
-    public void explode(){}
 
-    public void removeCharacter(Character character){}
+        }
+        return materialArray;
+    }
 
-    public void handleCountDown(){}
+    /**
+     * Ha az aszteroida magja üres, akkor a kapott nyersanyagot beteszi a magjába, egyébként kivételt dob.
+     */
+    public void addCore(Material material)throws CoreFullException {
+        if( coreMaterial == null){
+            coreMaterial = material;
+        }
+        else{
+            throw new CoreFullException("Core is already full");
+        }
+    }
 
+    /**
+     * Radioaktív robbanás következtében meghívja minden rajta tartózkodó radExplode() metódusát.
+     */
+    public void explode(){
+        for (Character character : charactersOnSurface) {
+            character.radExplode();
+        }
+    }
+
+    /**
+     * Ha a kapott karakter szerepel az aszteroidán lévők listájában, akkor kiveszi onnan.
+     */
+    public void removeCharacter(Character character){
+        if (charactersOnSurface.contains(character)){
+            charactersOnSurface.remove(character);
+        }
+    }
+
+    /**
+     * Csökkenti a napközelségi számlálót. Ha a számláló 0 lesz és a kéreg is át van fúrva, akkor meghívja
+     * a magban lévő nyersanyag closeToSunAction-jét, majd visszaállítja a számlálót.
+     */
+    public void handleCountDown(){
+        turnsTillCloseToSun = turnsTillCloseToSun -1;
+        if(turnsTillCloseToSun ==0){
+            if(layer ==0){
+                coreMaterial.closeToSunAction(this);
+
+            }
+            turnsTillCloseToSun = closeToSunFreq;
+        }
+    }
+
+    /**
+     * Ha a kapott karakter még nem szerepel az aszteroidán lévők listájában, akkor hozzáadja.
+     */
     @Override
     public void addCharacter(Character character) {
         if(!charactersOnSurface.contains(character)){
@@ -53,6 +133,9 @@ public class Asteroid implements IAsteroid {
         }
     }
 
+    /**
+     * A paraméterként kapott objektum eltávolítása a szomszédokból.
+     */
     @Override
     public void removeNeighbour(Asteroid asteroid) {
         neighbours.remove(asteroid);
@@ -73,9 +156,11 @@ public class Asteroid implements IAsteroid {
     public void setCoreMaterial(Material coreMaterial) {
         this.coreMaterial = coreMaterial;
     }
-    
+    //TODO
     public Asteroid getDrillableNeighbour(){}
 
+
     public List<IAsteroid> getNeighbours() {
+        return neighbours;
     }
 }
