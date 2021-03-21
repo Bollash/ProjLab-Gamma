8b1a5c7a22092d57ed12fb163033730a09d099ca
Ferenc Nyéki <nyekiferi99@edu.bme.hu>
2021-03-21 14:33:56 +0100

    uran kész

--- src/modell/Uran.java
+++ src/modell/Uran.java
@@ -3,11 +3,15 @@ package modell;
 public class Uran extends Material{
     @Override
     public MaterialType getType() {
+        System.out.println("Belépett a getType-ba");
+        System.out.println("Kilépett a getType-ból");
         return MaterialType.Uran;
     }
 
     @Override
     public void closeToSunAction(Asteroid ast) {
+        System.out.println("Belépett a closeToSunAction-be");
         ast.explode();
+        System.out.println("Kilépett a closeToSunAction-ból");
     }
 }
