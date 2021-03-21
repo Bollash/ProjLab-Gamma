5af55d787a611fde4c2022d4097e76b7c134d33a
Ferenc Nyéki <nyekiferi99@edu.bme.hu>
2021-03-21 14:31:57 +0100

    iron kész

--- src/modell/Iron.java
+++ src/modell/Iron.java
@@ -3,6 +3,8 @@ package modell;
 public class Iron extends Material{
     @Override
     public MaterialType getType() {
+        System.out.println("Belépett a getType-ba");
+        System.out.println("Kilépett a getType-ból");
         return MaterialType.Iron;
     }
 }
