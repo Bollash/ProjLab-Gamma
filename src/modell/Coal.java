f2d0a7fb11bf44f96f2ebaf06b115c20c2eea1cb
Ferenc Nyéki <nyekiferi99@edu.bme.hu>
2021-03-21 14:12:14 +0100

    Coal kész

--- src/modell/Coal.java
+++ src/modell/Coal.java
@@ -1,8 +1,14 @@
 package modell;
 
 public class Coal extends Material{
+    /**
+     * Ez a függvény visszaadja, hogy az adott material Coal
+     * @return az adott material Coal típusú
+     */
     @Override
     public MaterialType getType() {
+        System.out.println("Belépett a getType-ba");
+        System.out.println("Kilépett a getType-ból");
         return MaterialType.Coal;
     }
 }
