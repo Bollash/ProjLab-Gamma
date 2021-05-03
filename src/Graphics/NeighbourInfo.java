8bb03493d8f76c054fd745f1b0328e761541feee
nyekiferi <79425951+nyekiferi@users.noreply.github.com>
2021-05-03 13:57:27 +0200

    Astinfo nagyjából kész, nem teljesen tudom, hogy hogyan kéne úgy megcsinálni, hogy nem sok labelt, meg textbox-ot hozok létre, illetve a stringgé castolásban kérnék egy kis segítséget

---
+++ src/Graphics/NeighbourInfo.java
@@ -0,0 +1,13 @@
+package Graphics;
+
+import modell.Asteroid;
+import javax.swing.*;
+
+public class NeighbourInfo extends JFrame {
+    JButton list;
+    JComboBox neighbours;
+
+    public NeighbourInfo(Asteroid ast){
+
+    }
+}
---
+++ src/Graphics/SpaceInfo.java
@@ -0,0 +1,10 @@
+package Graphics;
+
+import modell.Space;
+import javax.swing.*;
+
+public class SpaceInfo extends JFrame {
+    public SpaceInfo(Space space){
+
+    }
+}
