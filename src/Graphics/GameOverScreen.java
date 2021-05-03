6fc3031e4c2a72509e8036216f8d8dcf6621eaf9
nyekiferi <79425951+nyekiferi@users.noreply.github.com>
2021-05-03 14:27:30 +0200

    GameOverScreen kész

---
+++ src/Graphics/GameOverScreen.java
@@ -0,0 +1,23 @@
+package Graphics;
+
+import javax.swing.*;
+
+public class GameOverScreen extends JFrame {
+    private JFrame f = new JFrame("SpaceInfo");
+
+    public GameOverScreen(String message){
+        JPanel p = new JPanel();
+
+        JLabel l = new JLabel(message);
+
+        p.add(l);
+
+        f.add(p);
+
+        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f.setSize(400, 110);
+        f.setResizable(false);
+        f.pack();
+        f.setVisible(true);
+    }
+}
