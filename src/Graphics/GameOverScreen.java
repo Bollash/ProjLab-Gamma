abb091b77bde97256465ce29857fadd60106e245
nyekiferi <79425951+nyekiferi@users.noreply.github.com>
2021-05-03 14:29:35 +0200

    GameOverScreen kész

--- src/Graphics/GameOverScreen.java
+++ src/Graphics/GameOverScreen.java
@@ -14,6 +14,8 @@ public class GameOverScreen extends JFrame {
 
         f.add(p);
 
+
+
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         f.setSize(400, 110);
         f.setResizable(false);
