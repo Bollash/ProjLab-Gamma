b411aac777ab8ca53edda2ee8ddf5d99850ee1dd
Bollash <scheierbalu@gmail.com>
2021-05-05 23:39:33 +0200

    remélhetőleg jó

--- src/Graphics/GameOverScreen.java
+++ src/Graphics/GameOverScreen.java
@@ -1,23 +1,28 @@
 package Graphics;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 
 public class GameOverScreen extends JFrame {
-    private JFrame f = new JFrame("SpaceInfo");
 
     public GameOverScreen(String message){
         JPanel p = new JPanel();
+        JPanel p2 = new JPanel();
+        p2.setLayout(new GridBagLayout());
 
         JLabel l = new JLabel(message);
-
+        l.setFont(new Font("arial", Font.BOLD, 20));
         p.add(l);
+        p2.add(p);
 
-        f.add(p);
-
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        f.setSize(400, 110);
-        f.setResizable(false);
-        f.pack();
-        f.setVisible(true);
+        this.add(p2);
+        this.setName("Game Over");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setSize(600, 300);
+        this.setResizable(false);
+        this.setVisible(true);
     }
 }
