2b3dd06dacdf089f1c38bab6a34c44067756a63d
szmida p <patriksz25@gmail.com>
2021-05-05 12:04:37 +0200

    xd

--- src/Graphics/GameScreen.java
+++ src/Graphics/GameScreen.java
@@ -2,8 +2,31 @@ package Graphics;
 
 import modell.Space;
 
-public class GameScreen {
-    public GameScreen(Space space) {
+import javax.swing.*;
+
+public class GameScreen extends JFrame {
+
+    Space space;
+    int currentActor;
+    JComboBox<String> info;
+    JComboBox<String> act;
+    JLabel infoLabel;
+    boolean settlerActing; //??
+    String[] infoCB = {"Space","Settler","Asteroid","Neighbour"};
+    String[] actCB = {"Build Teleport Gate","Build Robot","Build Base","Drill","Mine","Move","Put Material Back","Put Teleport gate down"};
+
+
+
+
+
+
+    public GameScreen(Space spaceField) {
+        space = spaceField;
+        currentActor = 0;
+        info = new JComboBox<>(infoCB);
+        act =  new JComboBox<>(actCB);
+        infoLabel = new JLabel();
+        settlerActing = false;
 
     }
 }
